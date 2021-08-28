d85f68a2270e86e1bf574fa19542d435b3db0a18
yokwejuste <yokwejuste@yahoo.com>
2021-08-28 12:21:00 +0100

    arrangements

--- records/urls.py
+++ records/urls.py
@@ -1,8 +1,9 @@
+from django.conf.urls import url
 from django.urls import path
 
 from records.views import register, login_go_user, \
     logout_go_user, choices, SearchResult, dashboard, \
-    profile, records, send_files_1, preview, send_files_2, send_files_3  # ,h404
+    profile, records, send_files_1, preview, send_files_2, send_files_3, h404  # ,h404
 
 urlpatterns = [
     path('', register, name='register'),
@@ -15,8 +16,8 @@ urlpatterns = [
     path('profile', profile, name='profile'),
     path('records', records, name='records'),
     path('preview', preview, name='preview'),
-    # path('query_life_is_good', h404, name='h404'),
-    # url(r'^$', h404, name='page_not_found'),
+    path('query_life_is_good', h404, name='h404'),
+    url(r'^$', h404, name='page_not_found'),
     path('upload_s', send_files_1, name='upload_s'),
     path('upload_t', send_files_2, name='upload_t'),
     path('upload_v', send_files_3, name='upload_v'),
